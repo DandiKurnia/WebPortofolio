0dcab973da4fc0143b9587da25caa57db13c6553
DandiKurnia <dandikurnia608@gmail.com>
2025-02-20 17:10:03 +0700

    routeRedirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
+        
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
